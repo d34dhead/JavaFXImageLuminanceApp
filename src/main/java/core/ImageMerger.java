d781fbd9f6ce15795ab799ce8c772faaacf4b903
d34dhead <34287749+d34dhead@users.noreply.github.com>
2018-03-25 17:00:37 +0200

    Added tests for multithreaded solution. Processing time down more than 3 times.

--- src/main/java/core/ImageMerger.java
+++ src/main/java/core/ImageMerger.java
@@ -2,6 +2,7 @@ package core;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 //1. store multiple images in an array
 //2. make a Llab matrix of each image
@@ -25,26 +26,52 @@ public class ImageMerger {
     }
 
 
-    //from given list of Llab matrioes, take the matrix with baseIndex and for each invalid Llab value, look for a valid value on the same index of a
+    //from given list of Llab matrioes, take the matrix with baseIndex and for each invalid Llab value in the matrix, look for a valid value on the same index in a
     // different matrix, then replace the invalid value with the valid one
+    // picks the value closest to Llab = 50, as that is where the best precision can be achieved
     public double[][] MergeLlabMatrices(int baseIndex, ArrayList<double[][]> llabMatrices) {
-        double[][] enhanced = llabMatrices.get(baseIndex);
+        //copy 2d array
+        double[][] enhanced = Arrays.stream(llabMatrices.get(baseIndex))
+                .map(double[]::clone)
+                .toArray(double[][]::new);
+        ArrayList<Double> potentialValues = new ArrayList<>();
 
-        for(int i = 0; i < enhanced[0].length; i++){
-            for(int j = 0; j < enhanced.length; j++){
-                //if value is invalid, look for a valid value in another matrix at the same index and replace
-                if(!validLValue(enhanced[j][i])){
-                    for(int k = 0; k < llabMatrices.size(); k++){
-                        if(k == baseIndex) continue;
+        for (int i = 0; i < enhanced[0].length; i++) {
+
+            for (int j = 0; j < enhanced.length; j++) {
+
+                //if value is invalid, look for a valid value in another matrix at the same index and replace with the best fit
+                if (!validLValue(enhanced[j][i])) {
+                    for (int k = 0; k < llabMatrices.size(); k++) {
+
+                        if (k == baseIndex) continue;
                         double lValue = llabMatrices.get(k)[j][i];
-                        if(validLValue(lValue)){
-                            enhanced[j][i] = lValue;
+                        if (validLValue(lValue)) {
+                            //enhanced[j][i] = lValue;
+                            potentialValues.add(lValue);
                         }
                     }
+                    if (potentialValues.isEmpty()) continue;
+
+                    enhanced[j][i] = closestTo50(potentialValues);
                 }
             }
         }
         return enhanced;
+    }
+
+    //find the number closest to 50
+    private double closestTo50(ArrayList<Double> potentialValues) {
+        double closest = potentialValues.get(0);
+
+        for (int i = 1; i < potentialValues.size(); i++) {
+            double currentValue = potentialValues.get(i);
+            if (Math.abs(closest - 50) > Math.abs(currentValue - 50)){
+                closest = currentValue;
+            }
+        }
+        potentialValues.clear();
+        return closest;
     }
 
     //returns a Llab matrix with the least amount of invalid pixels (<=20 or >=80)
@@ -54,25 +81,25 @@ public class ImageMerger {
         int cols = llabMatrices.get(0)[0].length;
         int rows = llabMatrices.get(0).length;
 
-        int lowestInvalidPixelCount = rows*cols;
+        int lowestInvalidPixelCount = rows * cols;
 
         for (int i = 0; i < llabMatrices.size(); i++) {
             double[][] current = llabMatrices.get(i);
             int invalidCount = this.countInvalidPixels(current);
 
-            if(invalidCount < lowestInvalidPixelCount) {
+            if (invalidCount < lowestInvalidPixelCount) {
                 lowestInvalidPixelCount = invalidCount;
                 leastInvalidPixelsMatrix = current;
             }
         }
-    return leastInvalidPixelsMatrix;
+        return leastInvalidPixelsMatrix;
     }
 
-    private boolean validLValue(double value){
+    private boolean validLValue(double value) {
         return !(value <= 20) && !(value >= 80);
     }
 
-    public int countInvalidPixels(double[][] matrix){
+    public int countInvalidPixels(double[][] matrix) {
         int count = 0;
 
         int cols = matrix[0].length;
@@ -80,7 +107,7 @@ public class ImageMerger {
 
         for (int i = 0; i < cols; i++) {
             for (int j = 0; j < rows; j++) {
-                if(!validLValue(matrix[j][i])) count++;
+                if (!validLValue(matrix[j][i])) count++;
             }
         }
 
--- src/test/java/core/ImageMergerTest.java
+++ src/test/java/core/ImageMergerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -15,9 +16,10 @@ public class ImageMergerTest {
     private ArrayList<double[][]> mergeTestMatrices;
     private BufferedImage[] images;
     private double[][] llabMatrix;
+    private double[][] expectedMergeResult;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp(){
         merger = new ImageMerger(new ImageProcessor());
         llabMatrix = new double[][]{{50, 50, 50}, {5, 6, 80}, {90, 80, 50}};
 
@@ -33,8 +35,7 @@ public class ImageMergerTest {
         mergeTestMatrices.add(new double[][]{{0, 0, 80, 90},{20, 77, 79, 40},{80, 85, 40, 5}});
         mergeTestMatrices.add(new double[][]{{80, 44, 5, 90},{8, 11, 79, 40},{80, 85, 40, 5}});
 
-
-
+        expectedMergeResult = new double[][]{{50, 55, 80, 60}, {55, 66, 79, 40}, {80, 55, 40, 5}};
     }
 
     @Test
@@ -49,11 +50,24 @@ public class ImageMergerTest {
 
     @Test
     public void mergeLlabMatrices() {
+        double[][] mergeResult = merger.MergeLlabMatrices(0, mergeTestMatrices);
+
+        System.out.println(Arrays.deepToString(mergeTestMatrices.get(0)));
+
         int beforeEnhancing = merger.countInvalidPixels(mergeTestMatrices.get(0));
-        int afterEnhancing = merger.countInvalidPixels(merger.MergeLlabMatrices(0,mergeTestMatrices ));
+        int afterEnhancing = merger.countInvalidPixels(mergeResult);
+
 
         assertEquals(6, beforeEnhancing);
         assertEquals(3, afterEnhancing);
+
+        for (int i = 0; i < mergeResult[0].length; i++) {
+            for (int j = 0; j < mergeResult.length; j++) {
+                assertEquals(expectedMergeResult[j][i], mergeResult[j][i], .1);
+            }
+        }
+
+
     }
 
 }
